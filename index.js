7243551a47469f019a19508bfe6ca3eda7bb7158
queniaVT <queniaVT@proton.me>
2026-08-09 17:30:53 +0200

    added gnu/linux copypasta

--- index.js
+++ index.js
@@ -23,7 +23,9 @@ const sig = "\n-# ⓘ This user is suspected to have illegally traveled in cross
 const crazy = /crazy/i;
 const j_b = /job/i;
 const six = /6/i;
-const seven = /7/i;;
+const seven = /7/i;
+const linux = /linux/i;
+const gnu = /gnu/i;
 
 const storage = path.join(path.dirname(fileURLToPath(import.meta.url)), "rrstore.json");
 const prefix = "/";
@@ -222,6 +224,9 @@ client.on(Events.MessageCreate, async (message) => {
 		};
 		if (six.test(message.content) && seven.test(message.content)) {
 			send(message, "HOLY MOTHER FUCKNG SHT, ARE THOSE THE NUMBERS 6 AND 7?!?!?!😱😳😱😳😳😱⁉️😱⁉️‼️😱😳😱⁉️😱😳😱😳⁉️😱😳😱⁉️😱‼️😱😳😱6️⃣7️⃣6️⃣7️⃣6️⃣7️⃣6️⃣7️⃣ ATTENTION, 6️⃣7️⃣ SPOTTED, ATTENTION 67 SPOTTED, THIS IS NOT A DRILL, I REPEAT, THIS IS NOT A DRILL DEPLOY 6️⃣7️⃣ PROTOCOL /INITIATING 67 MODE... %67data... &programs x67&... 6767676767676️⃣7️⃣6️⃣7️⃣6️⃣7️⃣... I WILL SING THE 6️⃣ 7️⃣ SONG AND YOU WILL SING ALONG, WE WILL SING THE 6️⃣ 7️⃣ SONG AND YOU WILL SING ALONG, YOU WILL SING THE 6️⃣ 7️⃣ SONG AND WE WILL SING ALONG 6️⃣🤚😁✋️7️⃣‼️‼️‼️‼️‼️‼️");
+		};
+		if (linux.test(message.content) && !gnu.test(message.content)) {
+			send(message, "I'd just like to interject for a moment. What you're refering to as Linux, is in fact, GNU/Linux, or as I've recently taken to calling it, GNU plus Linux. Linux is not an operating system unto itself, but rather another free component of a fully functioning GNU system made useful by the GNU corelibs, shell utilities and vital system components comprising a full OS as defined by POSIX.\n\nMany computer users run a modified version of the GNU system every day, without realizing it. Through a peculiar turn of events, the version of GNU which is widely used today is often called Linux, and many of its users are not aware that it is basically the GNU system, developed by the GNU Project.\n\nThere really is a Linux, and these people are using it, but it is just a part of the system they use. Linux is the kernel: the program in the system that allocates the machine's resources to the other programs that you run. The kernel is an essential part of an operating system, but useless by itself; it can only function in the context of a complete operating system. Linux is normally used in combination with the GNU operating system: the whole system is basically GNU with Linux added, or GNU/Linux. All the so-called Linux distributions are really distributions of GNU/Linux!");
 		};
 	};
 });
